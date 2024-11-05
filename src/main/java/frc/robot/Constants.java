d9919738e1f765d5f4f0e8816a762cccb71a8b28
Fauvodan <jpt88@outlook.com>
2024-11-05 09:26:21 -0800

    Added the drivetrain can ids

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -5,11 +5,34 @@ package frc.robot;
 public class Constants {
     
     //This is where you would put the controller ports.  Check the driver station for the number.
-    public static class OperatorConstants{
+    public static class OperatorConstants {
 
         /**
          * The port for the driver controller, regardless of what controller.
          */
         public static final int kDriverCPort = 0;
+    }
+
+    //This is where you would put the drivetrain motor CAN IDs
+    public static class DriveMotorConstants {
+        /**
+         * The CAN ID for the front left motor
+         */
+        public static final int fl = 0;
+
+        /**
+         * The CAN ID for the front right motor
+         */
+        public static final int fr = 1;
+
+        /**
+         * The CAN ID for the back left motor
+         */
+        public static final int bl = 2;
+
+        /**
+         * The CAN ID for the back right motor
+         */
+        public static final int br = 3;
     }
 }
