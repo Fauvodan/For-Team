087f235758e3859b64c6411cb81c13af132a5a81
Fauvodan <jpt88@outlook.com>
2024-11-05 09:47:49 -0800

    Added the acceleration values to the dashboard

--- src/main/java/frc/robot/Subsystems/DiagnosticsSub.java
+++ src/main/java/frc/robot/Subsystems/DiagnosticsSub.java
@@ -1,6 +1,7 @@
 package frc.robot.Subsystems;
 
 import edu.wpi.first.wpilibj.BuiltInAccelerometer;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.OutConstants;
 
@@ -21,10 +22,35 @@ public class DiagnosticsSub extends SubsystemBase {
      * Note that the roborio returns it in g.  I'm multiplying by g in order to return it as a normal number.
      */
 
+     /**
+      * An acceleration value.
+      * @return The x-value in ms^-2
+      */
      public double xaccel() {
         return accel.getX() * OutConstants.grav;
      }
 
+     /**
+      * An acceleration value.
+      * @return The y-value in ms^-2
+      */
+     public double yaccel() {
+        return accel.getY() * OutConstants.grav;
+     }
+
+     /**
+      * An acceleration value.
+      * @return The z-value in ms^-2
+      */
+     public double zaccel() {
+        return accel.getZ() * OutConstants.grav;
+     }
+
     //Runs periodically while the robot is running
-    public void periodic() {}
+    public void periodic() {
+        //Puts each acceleration value on the dashboard
+        SmartDashboard.putNumber("X Acceleration", xaccel());
+        SmartDashboard.putNumber("Y Acceleration", yaccel());
+        SmartDashboard.putNumber("Z Acceleration", zaccel());
+    }
 }
