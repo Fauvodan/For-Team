c1d47645d12b1a2007a5f43248f360bc499cc4ff
Fauvodan <jpt88@outlook.com>
2024-11-05 09:36:00 -0800

    Added a diagnostics subsystem

---
+++ src/main/java/frc/robot/Subsystems/DiagnosticsSub.java
@@ -0,0 +1,10 @@
+package frc.robot.Subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+//A subsystem that returns diagnostics about the robot.  
+//This is entirely trinkets and non-necessary, but you may find it helpful
+public class DiagnosticsSub extends SubsystemBase {
+    
+    
+}
