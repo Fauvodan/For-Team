c8c21c36fd830f96542158812d689197d7d6cac9
Fauvodan <jpt88@outlook.com>
2024-11-08 15:47:23 -0800

    Added encoders to diagnostic with reset methods, velocity, and left distance

--- src/main/java/frc/robot/Subsystems/DiagnosticsSub.java
+++ src/main/java/frc/robot/Subsystems/DiagnosticsSub.java
@@ -4,6 +4,7 @@ import edu.wpi.first.wpilibj.BuiltInAccelerometer;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.EncoderConstants;
 import frc.robot.Constants.OutConstants;
@@ -75,6 +76,59 @@ public class DiagnosticsSub extends SubsystemBase {
         return DriverStation.getMatchTime();
      }
 
+
+   /**
+    * Resets the left encoder
+    * @return A command to reset the left encoder
+    */
+    public Command lEncoderReset() {
+      return runOnce(() ->
+      {
+         leftEncoder.reset();
+      }
+      ); 
+    }
+
+    /**
+     * Resets the right encoder
+     * @return A command to reset the right encoder
+     */
+    public Command rEncoderReset() {
+      return runOnce(() ->
+      {
+         rightEncoder.reset();
+      });
+    }
+
+    /**
+     * Resets all encoders
+     * @return A command to reset all encoders
+     */
+    public Command allEncoderReset() {
+      return runOnce(() -> 
+      {
+         rightEncoder.reset();
+         leftEncoder.reset();
+      });
+    }
+
+
+     /**
+      * Distance calculation.  Note that that is the left encoder, so when the robot turns, it will be inaccurate.  You'll want to use a reset command.
+      * @return
+      */
+     public double lDistanceIn() {
+      return leftEncoder.getDistance();
+     }
+
+     /**
+      * Velocity of the robot in in/s
+      * @return the velocity of the robot
+      */
+     public double velocity() {
+      return leftEncoder.getRate();
+     }
+
     //Runs periodically while the robot is running
     public void periodic() {
         //Puts each acceleration value on the dashboard
@@ -83,5 +137,11 @@ public class DiagnosticsSub extends SubsystemBase {
         SmartDashboard.putNumber("Z Acceleration", zaccel());
         //Puts the FMS Time on the dashboard
         SmartDashboard.putNumber("FMS Time", fgaTime());
+
+        //The distance the left wheel has travelled
+        SmartDashboard.putNumber("Distance", lDistanceIn());
+
+        //The speed of the robot.  We're using an absolute value so that it can be displayed with a dial on the dashboard.
+        SmartDashboard.putNumber("Speed", Math.abs(velocity()));
     }
 }
